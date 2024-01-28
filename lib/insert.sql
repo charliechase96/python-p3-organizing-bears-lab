0a24b7b927e23fe3cdd5c99bfd82e033e3adbe58
Charlie Gray <charliechasegray@gmail.com>
2024-01-27 19:05:14 -0700

    finish lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Mr. Chocolate', 7, 'Male', 'Brown', 'Friendly', 1),
+    ('Rowdy', 3, 'Male', 'Black', 'Energetic', 1),
+    ('Tabitha', 5, 'Female', 'Golden', 'Calm', 1),
+    ('Sergeant Brown', 7, 'Male', 'Brown', 'Disciplined', 1),
+    ('Melissa', 4, 'Female', 'White', 'Playful', 1),
+    ('Grinch', 2, 'Male', 'Green', 'Mischievous', 1),
+    ('Wendy', 6, 'Female', 'Gray', 'Gentle', 1),
+    (NULL, 4, 'Female', 'Purple', 'Curious', 0);
